11aa840443e29491dd0ab7cd87cc9a837acaa935
Jonathan Wang <j2279wan@uwaterloo.ca>
2021-09-23 20:42:39 -0400

    Particles.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,40 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Particles from "react-particles-js";
 
 import theme from "util/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider theme={theme}>
+            <Particles
+                style={{
+                    position: "fixed",
+                    zIndex: -100,
+                    left: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "100%",
+                }}
+                params={{
+                    particles: {
+                        number: {
+                            value: 50,
+                        },
+                        size: {
+                            value: 3,
+                        },
+                    },
+                    interactivity: {
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                        },
+                    },
+                }}
+            />
             <Component {...pageProps} />
         </ChakraProvider>
     );
