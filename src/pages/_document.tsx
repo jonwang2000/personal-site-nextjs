97f2d63323fcbfa7229f8c3db7e3739f115c3b19
Jonathan Wang <j2279wan@uwaterloo.ca>
2021-09-23 20:34:45 -0400

    Default to dark mode

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, {
+    DocumentContext,
+    Html,
+    Head,
+    Main,
+    NextScript,
+} from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+
+import theme from "util/theme";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <ColorModeScript
+                        initialColorMode={theme.config.initialColorMode}
+                    />
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
